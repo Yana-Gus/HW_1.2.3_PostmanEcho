abab5b7c8f7298de50b211f6dc7934823802aa73
Yana G <ivana-1@mail.ru>
2023-07-06 21:19:35 +0300

    first commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    public void should () {
+        // Given - When - Then
+// Предусловия
+        ResponseAwareMatcher<Response> equalTo;
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello"))
+        ;
+    }
+}
